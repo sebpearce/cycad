2ecc91613945a4f3b5808338ebdfd3706757fd8d
sebpearce <sebpearce@gmail.com>
2017-11-08 20:27:04 +1100

    Remove filter method from repo class

--- lib/cycad/repo.rb
+++ lib/cycad/repo.rb
@@ -23,42 +23,6 @@ module Cycad
       def purge_all
         @transactions = []
       end
-
-      def filter(date_range: nil, type: nil, category_id: nil)
-        result = transactions
-
-        if date_range
-          raise ArgumentError, 'Wrong date range format' if
-            !date_range[:start_date] || !date_range[:end_date]
-
-          result = result.select do |transaction|
-            transaction.date > date_range[:start_date] &&
-            transaction.date < date_range[:end_date]
-          end
-        end
-
-        if type
-          result = result.select do |transaction|
-            if type == :income
-              transaction.amount > 0
-            elsif type == :expense
-              transaction.amount < 0
-            else
-              raise ArgumentError, 'Wrong transaction type format'
-            end
-          end
-        end
-
-        if category_id
-          raise ArgumentError, 'Wrong category format' if !category_id.is_a? Numeric
-
-          result = result.select do |transaction|
-            transaction.category_id == category_id
-          end
-        end
-
-        result
-      end
     end
   end
 end
