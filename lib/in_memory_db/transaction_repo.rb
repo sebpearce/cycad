a3915970f253df95a170bf9755ad09f62761df01
sebpearce <sebpearce@gmail.com>
2017-12-20 09:26:29 +1100

    Write in-memory TransactionRepo

--- lib/in_memory_db/transaction_repo.rb
+++ lib/in_memory_db/transaction_repo.rb
@@ -1,35 +1,75 @@
 module Cycad
   module InMemoryDB
     class TransactionRepo
+      Transaction = Struct.new(
+        :id,
+        :date,
+        :amount,
+        :category_id,
+        :note,
+        :tags
+      )
+
       attr_accessor :transactions
 
       def initialize
         @transactions = []
+        @id = 0
       end
 
-      def persist(transaction)
-        transactions << transaction
-        transaction
+      def create(args)
+        new_transaction = Transaction.new(
+          @id,
+          args[:date],
+          args[:amount],
+          args[:category_id],
+          args[:note],
+          args[:tags]
+        )
+        @transactions << new_transaction
+        @id += 1
+        new_transaction
       end
 
-      def find_by_id(id)
+      def by_id(id)
         transactions.find { |transaction| transaction.id == id }
       end
 
-      def update(transaction, args = {})
-        transaction.update(args)
+      def all
+        transactions
       end
 
-      def count
-        transactions.count
+      def delete(id)
+        transaction = by_id(id)
+        transactions.delete(transaction)
       end
 
-      def purge(id)
-        transactions.delete(id)
+      def update(id, args)
+        # TODO basic validation here and clean up the mess below
+        transaction = by_id(id)
+        transaction.date = args[:date] if args[:date] != nil
+        transaction.amount = args[:amount] if args[:amount] != nil
+        transaction.category_id = args[:category_id] if args[:category_id] != nil
+        transaction.note = args[:note] if args[:note] != nil
+        transaction.tags = args[:tags] if args[:tags] != nil
+        transaction
       end
 
-      def purge_all
-        @transactions = []
+      private
+
+      def update_amount
+      end
+
+      def update_date
+      end
+
+      def update_category
+      end
+
+      def update_note
+      end
+
+      def update_tags
       end
     end
   end
