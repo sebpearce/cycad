ac51e6cf43604da3b50cfe1b0f4c3116cf51f79f
sebpearce <sebpearce@gmail.com>
2018-02-19 22:21:17 +1100

    Implement transaction filters (except tags)

--- lib/database/transaction_repo.rb
+++ lib/database/transaction_repo.rb
@@ -1,4 +1,5 @@
 require 'rom-repository'
+require 'pry'
 
 module Database
   class TransactionRepo < ROM::Repository[:transactions]
@@ -6,45 +7,58 @@ module Database
     commands :create, update: :by_pk, delete: :by_pk, mapper: :transaction
 
     def query(conditions = true, &block)
-      transactions.where(conditions, &block)
+      transactions.where(conditions, &block).to_a
     end
 
-    def filter(amount:, date:, category_id:, tags:)
+    def filter(amount: nil, date: nil, category_id: nil, tags: nil)
       filtered = transactions
-      filtered = amount_filter(filtered, **amount)
+      filtered = amount_filter(filtered, **amount) if amount
+      filtered = date_filter(filtered, **date) if date
+      filtered = category_filter(filtered, category_id) if category_id
       filtered.to_a
     end
 
-    def amount_filter(filtered = transactions, lt: nil, gt: nil)
-      if lt
-        filtered = filtered.where { amount < lt }
-      end
+    def amount_filter(filtered = transactions, le: nil, ge: nil)
+      filtered = filtered.where { amount <= le } if le
+      filtered = filtered.where { amount >= ge } if ge
+      filtered
+    end
 
-      if gt
-        filtered = filtered.where { amount > gt}
-      end
+    def date_filter(filtered = transactions, from: nil, to: nil)
+      filtered = filtered.where { date <= to } if to
+      filtered = filtered.where { date >= from } if from
+      filtered
+    end
 
+    def category_filter(filtered = transactions, category_id)
+      filtered = filtered.where(category_id: category_id)
       filtered
     end
 
     def income_only
-      transactions.where { amount > 0 }.map_to(Cycad::Transaction).to_a
+      transactions.where { amount > 0 }.to_a
     end
 
     def expenses_only
-      transactions.where { amount < 0 }.map_to(Cycad::Transaction).to_a
+      transactions.where { amount < 0 }.to_a
     end
 
     def by_id(id)
-      transactions.by_pk(id).map_to(Cycad::Transaction).one
+      transactions.by_pk(id).one
     end
 
     def all
-      transactions.map_to(Cycad::Transaction).to_a
+      transactions.to_a
     end
 
     def delete_all
       transactions.delete
+    end
+
+    private
+
+    def transactions
+      super.map_to(Cycad::Transaction)
     end
   end
 end
--- spec/database/transaction_repo_spec.rb
+++ spec/database/transaction_repo_spec.rb
@@ -60,33 +60,91 @@ RSpec.describe Database::TransactionRepo, db: true do
 
   context '.filter' do
     it 'filters records over a given amount' do
-      result = subject.filter { amount > 5 }
+      result = subject.filter(amount: {ge: 5})
       expect(result).to be_an(Array)
       expect(result.first.amount).to eq(500)
       # expect(result).to be_an(Array)
       # expect(result).to include @record
     end
 
-    it 'filters records as an OR of two conditions' do
-      result = subject.filter { (amount < 300) | (amount > 450) }
-      wrong_result = subject.filter { (amount > 600) | (amount < 450) }
+    it 'filters records of an exact amount' do
+      subject.create(
+        date: Date.new(2018, 1, 1),
+        amount: 501,
+        category_id: @bills.id
+      )
+      result = subject.filter(
+        amount: {ge: 500, le: 500}
+      )
+      expect(result).to be_an(Array)
+      expect(result.length).to eq(1)
+      expect(result.first.amount).to eq(500)
+    end
+
+    it 'filters records between two amounts' do
+      result = subject.filter(amount: {le: 700, ge: 450})
+      wrong_result = subject.filter(amount: {ge: 600, le: 450})
       expect(result).to be_an(Array)
       expect(result.first.amount).to eq(500)
       expect(wrong_result).to be_empty
     end
 
-    it 'filters records as an AND of two conditions' do
-      result = subject.filter { (date > Date.parse('2017-01-01')) && (date < Date.parse('2017-12-31')) }
-      wrong_result = subject.filter { (date > Date.parse('2017-01-01')) && (date < Date.parse('2017-03-31')) }
+    it 'filters records between two dates' do
+      result = subject.filter(
+        date: {
+          from: Date.parse('2017-01-01'),
+          to: Date.parse('2017-12-31')
+      })
+      wrong_result = subject.filter(
+        date: {
+          from: Date.parse('2017-01-01'),
+          to: Date.parse('2017-03-31')
+        }
+      )
       expect(result).to be_an(Array)
       expect(result.first.amount).to eq(500)
       expect(wrong_result).to be_empty
     end
 
     it 'filters records since a given date' do
-      result = subject.filter { date > Date.parse('2017-01-01') }
-      wrong_result = subject.filter { date > Date.parse('2017-08-01') }
+      result = subject.filter(
+        date: {
+          from: Date.parse('2017-01-01')
+        }
+      )
+      wrong_result = subject.filter(
+        date: {
+          from: Date.parse('2017-08-01')
+        }
+      )
       expect(result).to be_an(Array)
+      expect(result.first.amount).to eq(500)
+      expect(wrong_result).to be_empty
+    end
+
+    it 'filters records on a single date' do
+      subject.create(
+        date: Date.new(2017, 5, 8),
+        amount: -300,
+        category_id: @bills.id
+      )
+      result = subject.filter(
+        date: {
+          from: Date.new(2017, 5, 7),
+          to: Date.new(2017, 5, 7)
+        }
+      )
+      expect(result.length).to eq(1)
+      expect(result.first.amount).to eq(500)
+    end
+
+    it 'filters records by category' do
+      result = subject.filter(
+        category_id: @bills.id
+      )
+      wrong_result = subject.filter(
+        category_id: @bills.id + 1
+      )
       expect(result.first.amount).to eq(500)
       expect(wrong_result).to be_empty
     end
